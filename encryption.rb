8b61f1bcfa6147302ba80ad8c9582f2dfb9d8e51
juwpan <juwpan@gmail.com>
2022-04-07 10:22:10 +0400

    'SHA2_2'

--- encryption.rb
+++ encryption.rb
@@ -4,12 +4,12 @@ puts 'Введите слово или фразу для шифрования:'
 
 word = gets.chomp
 
-puts 'Каким способом зашифровать:', '1. MD5', '2. SHA1'
+puts 'Каким способом зашифровать:', '1. MD5', '2. SHA1', '3. SHA2'
 
 user = gets.chomp
 
-if user == '1'
-  puts Digest::MD5.hexdigest(word)
-elsif user == '2'
-  puts Digest::SHA1.hexdigest(word)
+case user
+when '1' then puts Digest::MD5.hexdigest(word)
+when '2' then puts Digest::SHA1.hexdigest(word)
+when '3' then puts Digest::SHA2.hexdigest(word)
 end
