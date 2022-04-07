1f287077fcf8652df5eb84a57f00dbd914c6f33b
juwpan <juwpan@gmail.com>
2022-04-07 10:16:34 +0400

    sha1

--- encryption.rb
+++ encryption.rb
@@ -1,3 +1,15 @@
+require "digest"
+
 puts 'Введите слово или фразу для шифрования:'
 
+word = gets.chomp
+
 puts 'Каким способом зашифровать:', '1. MD5', '2. SHA1'
+
+user = gets.chomp
+
+if user == '1'
+  puts Digest::MD5.hexdigest(word)
+elsif user == '2'
+  puts Digest::SHA1.hexdigest(word)
+end
